8eebe2a4e8632b4ed280ed4aa935962ccda33d9d
Omer Barel <barelomer@gmail.com>
2018-01-11 20:51:12 +0200

    Delete userdata.sh

--- CloudFormation/userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export PATH=/usr/local/bin:$PATH;
-yum update
-yum install docker -y
-service docker start
-usermod -a -G docker ec2-user
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-chown root:docker /usr/local/bin/docker-compose
